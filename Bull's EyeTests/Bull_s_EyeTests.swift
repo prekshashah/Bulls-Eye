28340f3da3e078ffaa69fc49b4804bb6f15040c3
Ashiv Shah <Ashiv@Ashivs-MacBook-Air-2.local>
2014-12-31 16:00:04 +0530

    Initial Commit

---
+++ Bull's EyeTests/Bull_s_EyeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bull_s_EyeTests.swift
+//  Bull's EyeTests
+//
+//  Created by Ashiv Shah on 31/12/14.
+//  Copyright (c) 2014 Preksha Shah. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bull_s_EyeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
